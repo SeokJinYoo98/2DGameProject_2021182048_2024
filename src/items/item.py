0ab401d02b7435f10c6b53927adbeb4976d355e2
YSJ\you84 <you8343@gmail.com>
2024-11-07 12:42:33 +0900

    Added: Item class 1. 기본적인으로 필요한 함수 구조 생성 2. Item을 상속한 vaccine, coin class 추가

--- src/items/item.py
+++ src/items/item.py
@@ -3,7 +3,17 @@ import gfw
 class Item(gfw.Sprite):
     def __init__(self, x, y, fileName):
         super().__init__(fileName, x, y)
-
+        
+    def draw(self):
+        pass
+    def update(self):
+        pass
+    def magnetic(self):
+        pass
+    def get_bb(self):
+        pass
+    def special_function(self):
+        pass
 
 class Vacine(Item):
     pass
